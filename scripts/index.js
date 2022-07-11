c9cbb6ec12190c757bbbd8e898a433d7f598ef59
Алексей Филимонов <alexeyf08@gmail.com>
2022-07-11 21:07:38 +0700

    feat: релизная версия проекта

---
+++ scripts/index.js
@@ -0,0 +1,29 @@
+const openPopupButton = document.querySelector('.profile__edit-button');
+const closePopupButton = document.querySelector('.popup__button-close');
+const popup = document.querySelector('.popup');
+let PopupNameInput = document.getElementById('popup__profile-name');
+let PopupJobInput = document.getElementById('popup__profile-job');
+let formElement = document.querySelector('.popup__form');
+let ProfileName = document.querySelector('.profile__name');
+let ProfileJob = document.querySelector('.profile__job');
+
+function openPopup() {
+  popup.classList.add('popup_opened')
+  PopupNameInput.value = ProfileName.textContent;
+  PopupJobInput.value = ProfileJob.textContent;
+}
+
+function closePopup() {
+  popup.classList.remove('popup_opened')
+}
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  ProfileName.textContent = PopupNameInput.value;
+  ProfileJob.textContent = PopupJobInput.value;
+  closePopup()
+}
+
+openPopupButton.addEventListener('click', openPopup);
+closePopupButton.addEventListener('click', closePopup);
+formElement.addEventListener('submit', formSubmitHandler); 
